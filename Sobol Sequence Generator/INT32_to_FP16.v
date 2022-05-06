982b81dbd62fb9afb1627681babfc0b62ab5ad49
Chenyungchin <b08901061@ntu.edu.tw>
2022-05-06 11:21:34 +0800

    fix bug

--- Sobol Sequence Generator/INT32_to_FP16.v
+++ Sobol Sequence Generator/INT32_to_FP16.v
@@ -1,6 +1,6 @@
 // transfer the INT32 value from Sobol to FP16
-// modified FP16 scheme: 4 bits for exponent and 12 bits for mantissa (no sign bit since there are only positive values)
-// exponent: the bit of the most significant one (be 11 if MSO <= 11)
+// modified FP16 scheme: 5 bits for exponent and 11 bits for mantissa (no sign bit since there are only positive values)
+// exponent: the bit of the most significant one (be 10 if MSO <= 10)
 // mantissa: counting 12 bits starting from exponent 
 module INT32_to_FP16(
     input  [31:0] int32_val,
@@ -10,8 +10,8 @@ module INT32_to_FP16(
 // ========== parameters ==============
 integer i;
 // ========== wire and reg ============
-wire  [3:0] exponent;
-wire [11:0] mantissa;
+wire  [4:0] exponent;
+wire [10:0] mantissa;
 // most significant one
 reg   [4:0] MSO;
 
@@ -19,11 +19,11 @@ reg   [4:0] MSO;
 assign fp16_val = {exponent, mantissa};
 
 assign exponent = MSO;
-assign mantissa = int32_val[MSO -: 12];
+assign mantissa = int32_val[MSO -: 11];
 
 // ========== Combinational ===========
 always @(*) begin
-    MSO = 11;
+    MSO = 10;
     for (i=12; i<32; i=i+1) begin
         if (int32_val[i] == 1) MSO = i;
     end
