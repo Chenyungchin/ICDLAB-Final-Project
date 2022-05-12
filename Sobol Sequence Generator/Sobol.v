6593a04954ecdb98bf4b09cbba7c01dd47780cd0
Chenyungchin <b08901061@ntu.edu.tw>
2022-05-12 11:09:04 +0800

    modifiy Sobol.v architecture

--- Sobol Sequence Generator/Sobol.v
+++ Sobol Sequence Generator/Sobol.v
@@ -2,19 +2,13 @@ module Sobol(
     input             clk,
     input             rst_n,
     input             start,
-    output reg [15:0] res_fp16
+    output     [15:0] icdf
 );
 
 // ========== wire and reg ===================
 wire [31:0] res_int32;
-wire [15:0] res_fp16_w;
 
 // ========== Sequential =====================
-always @(posedge clk or negedge rst_n) begin
-    if (!rst_n) res_fp16 <= 16'b0;
-    else        res_fp16 <= res_fp16_w;
-end
-
 
 // ========== module instantiation ===========
 Sobol_to_INT32 sobol_to_int32_0(
@@ -24,9 +18,11 @@ Sobol_to_INT32 sobol_to_int32_0(
     .res(res_int32)
 );
 
-INT32_to_FP16 int32_to_fp16_0(
-    .int32_val(res_int32),
-    .fp16_val(res_fp16_w)
+ICDF_LUT icdf_lut0(
+    .clk(clk),
+    .rst_n(rst_n),
+    .cdf(res_int32),
+    .icdf(icdf)
 );
 
 endmodule
